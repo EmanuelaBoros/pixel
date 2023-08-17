ae7a9f63736b5f9742e8a3d421d3ad882c20cc50
Emanuela Boros <emanuela.boros@epfl.ch>
2023-08-17 12:36:59 +0000

    add configs

--- configs/config_maps.py
+++ configs/config_maps.py
@@ -1,7 +1,9 @@
 MODEL_PROTOTYPE_CONFIGS = {
-    "scratch_noto_span0.25-dropout": "configs/models/scratch_noto_span0.25-dropout.json"
+    "scratch_noto_span0.25-dropout": "configs/models/scratch_noto_span0.25-dropout.json",
+    "scratch_noto_span0.25-dropout_try01": "configs/models/scratch_noto_span0.25-dropout_try01.json"
 }
 
 TRAINING_CONFIGS = {
-    "fp16_apex_bs32": "configs/training/fp16_apex_bs32.json"
+    "fp16_apex_bs32": "configs/training/fp16_apex_bs32.json",
+    "fp16_apex_bs32_try01": "configs/training/fp16_apex_bs32_try01.json"
 }
